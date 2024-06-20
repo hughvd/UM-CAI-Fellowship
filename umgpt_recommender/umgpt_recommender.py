85c67ef1e817e4ab9208c448f0b8c6ea62a21fbb
hughvd <90873843+hughvd@users.noreply.github.com>
2024-06-20 13:51:50 -0400

    implementation and testing of embedding recommender

--- umgpt_recommender/umgpt_recommender.py
+++ umgpt_recommender/umgpt_recommender.py
@@ -1,9 +1,10 @@
+import numpy as np
 import pandas as pd
 from openai import AzureOpenAI
 import os
 from dotenv import load_dotenv
 from typing import List, Optional
-
+import heapq
 
 
 
@@ -78,7 +79,7 @@ class Recommender(object):
 
         num_cycles = 0
         print('Begin filter loop')
-        while filtered_df.shape[0] > 150 and num_cycles < 4:
+        while filtered_df.shape[0] > 500 and num_cycles < 2:
             print('Filtering...')
             messages.append({"role": "user", "content": 'Return additional keywords not listed above to filter dataframe.'})
             gpt_response = self.client.chat.completions.create(
@@ -125,7 +126,6 @@ class Recommender(object):
             stop=None)
         
         print('Returning...')
-        print(recommendation.choices[0].message.content)
         return recommendation.choices[0].message.content
     
 
@@ -161,6 +161,86 @@ class EmbeddingRecommender(object):
             azure_endpoint=os.environ['OPENAI_API_BASE'],
             organization=os.environ['OPENAI_ORGANIZATION_ID']
         )
-    
-    def generate_embedding():
-        """Generates embedding vectors for each course description in the dataframe."""
+    # Notes on optimizing:
+    # Currently using the pandas array to access each embedding vector, to compute most similar courses 
+    # by saving it as a matrix and getting row index of highest similarity would be more efficient.
+    # TODO:
+    # Metadata filtering to allow for more precise filtering?
+    def recommend(self, levels: Optional[List[int]] = None, query: str = ''):
+        print('Recommending...')
+        system_content = '''
+        Return an example course description of a course that would be most applicable to the following students request.
+        Student request: 
+        '''
+        system_content = system_content + query
+        messages = [
+                {"role": "system", "content": system_content},
+                {"role": "user", "content": query}
+            ]
+        print('Example description')
+        gpt_response = self.client.chat.completions.create(
+            model=os.environ['OPENAI_MODEL'],
+            messages=messages,
+            temperature=0,
+            stop=None).choices[0].message.content
+        print(gpt_response)
+        if levels is None:
+            levels = []
+        
+        if levels:
+            filtered_df = self.df[self.df['level'].isin(levels)]
+        else:
+            filtered_df = self.df
+        print(f"Initial size: {filtered_df.shape[0]}")
+        ## Turn the remaining courses into one long string.
+
+        ex_embedding = self.client.embeddings.create(
+            input = [gpt_response], 
+            model=os.environ['OPENAI_EMBEDDING_MODEL']).data[0].embedding
+        
+        # Get the top 100 similar courses.
+        # Loop through dataframe and keeping a stack of top 100 similar courses
+        # research running average algorithms
+        heap = []
+        for idx, row in filtered_df.iterrows():
+            # Get cosine similarity of i-th course description and example course.
+            similarity = cosine_similarity(ex_embedding, row['embedding'])
+            if idx < 100:
+                heapq.heappush(heap, (similarity, idx))
+            else:
+                heapq.heappushpop(heap, (similarity, idx))
+        
+        # Extract indexes
+        indexes = [idx for sim, idx in heap]
+
+        # If you want the actual vectors or rows, you can use:
+        filtered_df = filtered_df.iloc[indexes]
+        
+        course_string = ''
+
+        for _, row in filtered_df.iterrows():
+            course_name = row['course']
+            description = row['description']
+            course_string += f"Course {course_name}: {description}\n"
+
+        system_rec_message = "You are the worlds most highly trained academic advisor, a student has come to you with the following request: \n"
+        system_rec_message = system_rec_message + query + '\n'
+        system_rec_message = system_rec_message + '''Recommend the best courses from the following list, 
+                                                    return your recommendations as a list of the courses and a short rationale:\n''' + course_string
+        recommendation = self.client.chat.completions.create(
+            model=os.environ['OPENAI_MODEL'],
+            messages=[
+                {'role': 'system', 'content': system_rec_message}
+                ],
+            temperature=0,
+            stop=None)
+        
+        print('Returning...')
+        return recommendation.choices[0].message.content
+
+# HELPER FUNCTIONS
+def cosine_similarity(vec1, vec2):
+    dot_product = np.dot(vec1, vec2)
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+    return dot_product / (norm_vec1 * norm_vec2)
